28bca0fd9e236e91ba7f81ca04b1b3a2a9b19f99
zhiyuan <canju521968@sina.com>
2019-10-27 21:13:32 +0800

    add eui-input auto-complete...

--- src/main/resources/public/eui_form/input.js
+++ src/main/resources/public/eui_form/input.js
@@ -8,6 +8,7 @@ $(function(){
 		  advice1 :'',
 		  advice2 :'',
 		  advice3 :'',
+		  advice4 :'',
 		  restaurants: []
 	  },
 	  methods: {
@@ -18,10 +19,25 @@ $(function(){
 	          // 调用 callback 返回建议列表的数据
 	          cb(results);
 	        },
+	        querySearchAsyn(queryString, cb) { //这个函数是个奇葩存在..
+		        console.log("enter into qs=" + queryString + "|" + "callback=" +cb);
+	            var restaurants = this.restaurants;
+	            var results = queryString ? restaurants.filter(this.createStateFilter(queryString)) : restaurants;
+
+	            clearTimeout(this.timeout);
+	            this.timeout = setTimeout(() => {
+	              cb(results);
+	            }, 3000 * Math.random());
+	        },
 	        createFilter(queryString) {
 	          return (restaurant) => {
 	            return (restaurant.value.toLowerCase().indexOf(queryString.toLowerCase()) === 0);
 	          };
+	        },
+	        createStateFilter(queryString) {
+	            return (advice4) => {
+	              return (advice4.value.toLowerCase().indexOf(queryString.toLowerCase()) === 0);
+	            };
 	        },
 	        handleSelect(item) {
 	          console.log(JSON.stringify(item));
