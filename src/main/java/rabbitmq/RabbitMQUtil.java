a379965d54c42322dd8e0bd6f382b68dadc82823
zhiyuan <canju521968@sina.com>
2019-10-28 19:46:26 +0800

    add understand the diffenence between rabbitMQ.ZeroMQ,ActiveMQ..

---
+++ src/main/java/rabbitmq/RabbitMQUtil.java
@@ -0,0 +1,17 @@
+package rabbitmq;
+
+import javax.swing.JOptionPane;
+
+import cn.hutool.core.net.NetUtil;
+
+public class RabbitMQUtil {
+    public static void main(String[] args) {
+        checkServer();
+    }
+    public static void checkServer() {
+        if(NetUtil.isUsableLocalPort(15672)) {
+            JOptionPane.showMessageDialog(null, "RabbitMQ 服务器未启动 ");
+            System.exit(1);
+        }
+    }
+}
