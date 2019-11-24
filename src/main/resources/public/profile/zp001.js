e6735e69ea60cc769b2afeb247de036277c25b2a
zhiyuan <canju521968@sina.com>
2019-11-24 21:06:20 +0800

    add zp0010profile draft...

---
+++ src/main/resources/public/profile/zp001.js
@@ -0,0 +1,28 @@
+$(function(){
+	new Vue({
+		el:"#head",
+		data:{
+			lifePhoto:[
+				{"id":1,
+				 "url" :"./1.jpg"
+				},
+				{"id":2,
+				 "url" :"./2.jpg"
+				},
+				{"id":3,
+				 "url" :"./3.jpg"
+				},
+				{"id":4,
+				 "url" :"./4.jpg"
+				},
+				{"id":5,
+				 "url" :"./5.jpg"
+				},
+				{"id":6,
+				 "url" :"./6.jpg"
+				},
+			]
+		}
+		
+	})	
+})
