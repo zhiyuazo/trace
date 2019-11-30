f263f03a0f4957ecb585683d0a8be0d345c4b6f1
zhiyuazo <zhiyuazo>
2019-11-30 15:40:06 +0800

    sync@20191130

---
+++ src/main/java/NativeMethod/joriginal.java
@@ -0,0 +1,19 @@
+package NativeMethod;
+
+public class joriginal {
+	
+	public static void main(String[] args) throws InterruptedException {
+		joriginal  ins  = new joriginal();
+		long start = System.currentTimeMillis();
+		for (int i = 0 ; i < 1000 * 10000;i++) {
+			ins.computeSum(i, i+1);
+		}
+		long end = System.currentTimeMillis();
+		System.out.println("cost: " + (end -start));
+	}
+	
+	public  int computeSum(int a, int b) {
+		int res = a + b ;
+		return res; 
+	}
+}
