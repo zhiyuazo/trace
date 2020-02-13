27ba0764902cca126fa3c302ce5200a3cbdc45d4
zhiyuazo <zhiyuazo>
2020-02-13 22:11:15 +0800

    add lucene-demo...

---
+++ src/main/java/lucene/AnalyzerTest.java
@@ -0,0 +1,20 @@
+package lucene;
+
+import java.io.IOException;
+
+import org.apache.lucene.analysis.TokenStream;
+import org.wltea.analyzer.lucene.IKAnalyzer;
+
+public class AnalyzerTest {
+
+	public static void main(String[] args) throws IOException {
+		
+        IKAnalyzer analyzer = new IKAnalyzer();
+        TokenStream ts= analyzer.tokenStream("name", "里根号-军用目标-PLA103");
+        ts.reset();
+        while(ts.incrementToken()){
+            System.out.println(ts.reflectAsString(false));
+        }
+		
+	}
+}
