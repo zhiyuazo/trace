28b3b0defbe0793489c2c6721e431a214502f773
zhiyuazo <zhiyuazo>
2020-02-17 21:15:46 +0800

    add DBpool..more

---
+++ src/main/java/db/C3P0Pool.java
@@ -0,0 +1,39 @@
+package db;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javax.management.RuntimeErrorException;
+import javax.sql.DataSource;
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+public class C3P0Pool {
+//    public static ComboPooledDataSource dataSource=new ComboPooledDataSource("lqg");
+    public static ComboPooledDataSource dataSource=new ComboPooledDataSource();
+    
+	//获得数据源(连接池)
+    public static DataSource getDataSource() {
+        return dataSource;
+    }
+    //获得连接
+    public static Connection getConnection() {
+        try {
+            return dataSource.getConnection();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public static void main(String[] args) {
+    	String sql = "select * from t1";
+    	try(Connection  conn = C3P0Pool.getConnection() ;
+    		Statement s = conn.createStatement();
+    	){
+    		ResultSet rs = s.executeQuery(sql);
+    		while(rs.next()) {
+    			System.out.println(rs.getString(1));
+    		}
+    	}catch (Exception e) {
+			// TODO: handle exception
+		}
+    	
+	}
+}
