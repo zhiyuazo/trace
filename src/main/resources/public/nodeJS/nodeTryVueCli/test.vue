8ee17e3cfeb1bb490b9a78846bcc747eda5ea061
zhiyuazo <zhiyuazo>
2020-03-19 20:36:01 +0800

    add nodejs-vue-cli try...

---
+++ src/main/resources/public/nodeJS/nodeTryVueCli/test.vue
@@ -0,0 +1,22 @@
+<template>
+  <p>{{greeting}}  Vue-Cli</p>
+</template>
+ 
+<script>
+ 
+export default {
+    data () {
+        return {
+            greeting: 'Hi'
+        }
+    }
+}
+</script>
+ 
+<style scoped>
+    p{
+        font-size: 2em;
+        text-align:center;
+        color:blue;
+    }
+</style>
