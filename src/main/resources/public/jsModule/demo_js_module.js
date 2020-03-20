a00b8d08ed25060d7ddfd365925babb28288175c
zhiyuazo <zhiyuazo>
2020-03-20 22:00:05 +0800

    add  js import export demo

---
+++ src/main/resources/public/jsModule/demo_js_module.js
@@ -0,0 +1,9 @@
+import {say} from "./modules/a.js";
+
+$(function(){
+	console.log("A")
+	say();
+	console.log("B")
+	
+	
+})
---
+++ src/main/resources/public/jsModule/modules/a.js
@@ -0,0 +1,9 @@
+function say(){
+	console.log("zzy hello");
+}
+
+console.log("zzy hi");
+
+export {
+	say
+}
