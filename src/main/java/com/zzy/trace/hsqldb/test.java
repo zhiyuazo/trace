4b98d833ab11093d7dbb6b5ce04e7d8e3cb1096c
zhiyuazo <506663636@qq.com>
2020-11-18 22:12:45 +0800

    add some common js and css ..

---
+++ src/main/java/com/zzy/trace/hsqldb/test.java
@@ -0,0 +1,11 @@
+package com.zzy.trace.hsqldb;
+
+import org.hsqldb.util.DatabaseManagerSwing;
+public class test {
+    public static void main(String[] args) {
+        DatabaseManagerSwing.main(args);
+    }
+
+}
+
+ 
